cc1ae6e211aab4d6e18e378c449545bb0b3d640f
Cynthia Montalvo <129777144+montalcy@users.noreply.github.com>
2025-03-17 17:33:54 +0000

    Adding in files, still need media query

---
+++ js/script.js
@@ -0,0 +1,32 @@
+const firstName = "Cynthia";  
+const lastName = "Montalvo";  
+const delay = 150; 
+
+let firstNameIndex = 0;
+let lastNameIndex = 0;
+
+function animateText() {
+  document.getElementById("first-name").textContent = firstName.substring(0, firstNameIndex);
+  firstNameIndex++;
+
+  if (firstNameIndex > firstName.length) {
+    document.getElementById("last-name").textContent = lastName.substring(0, lastNameIndex);
+    lastNameIndex++;
+  }
+
+  if (lastNameIndex > lastName.length) {
+    clearInterval(interval);
+  }
+}
+
+const interval = setInterval(animateText, delay);
+
+
+const description = "Web Developer";
+const pop_up_delay = 100;
+
+function animateDesc(){
+    document.getElementById("description").textContent;
+
+    
+}
